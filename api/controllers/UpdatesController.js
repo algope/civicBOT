2dff16f7b39472176a6e0323f72dc2b24b718259
Alejandro <alejandro.gon.per@gmail.com>
2015-11-25 12:52:12 +0100

    New answers and bug fixes

--- api/controllers/UpdatesController.js
+++ api/controllers/UpdatesController.js
@@ -7,63 +7,68 @@
 
 module.exports = {
 
-  update: function(req, res){
-    var update = req.body;
-    var userId = update.message.from.id;
-    var userName = update.message.from.first_name;
-    if(update.message.text){
-      var text = update.message.text;
+    update: function (req, res) {
+        var update = req.body;
+        var userId = update.message.from.id;
+        var userName = update.message.from.first_name;
+        if (update.message.text) {
+            var text = update.message.text;
 
-      sails.log("Message: ",update.message.text);
+            sails.log.debug("Message: ", update.message.text);
+            var command = commands.processIt(text);
+            sails.log.debug("Command: ", command);
 
-      var command = commands.processIt(text);
+            if (command) {
+                telegram.sendMessage(userId, "Comando procesado correctamente, commandID: " + command).then(
+                    function (response) {
 
-      console.log("Command: ",command);
-      if (command){
-        telegram.sendMessage(userId, "Comando procesado correctamente, commandID: "+command).then(
-          function(response) {
+                    }, function (error) {
+                        sails.log.error("Failed", error);
+                    }
+                );
+            }
 
-          }, function(error) {
-            sails.log.error("Failed", error);
-          }
-        );
-      }
+        } else if (update.message.photo) {
+            sails.log.info("Photo content: ", update.message.photo);
+            telegram.sendMessage(userId, "Ahora clasifica la foto...", true, null, null).then(
+                function (response) {
 
-    }
+                }, function (error) {
+                    sails.log.error("Failed", error);
+                }
+            );
+            telegram.sendMessage(userId, "Si la información está relacionada con:\
+          Campañas de comunicación institucionales o con medios de comunicación, pulsa A.\
+          Acceso y permanencia en el sistema educativo o con el Tercer Sector, pulsa B.\
+          Reducción de las desigualdades y la exclusión social, pulsa C.\
+          Transparencia, participación ciudadanía o rendición de cuentas, pulsa D.", true, null, keyboards.createKeyboard()).then(
+                function (response) {
 
-    if(update.message.photo){
-      sails.log.info("Photo content: ", update.message.photo);
-      telegram.sendMessage(userId, "Ahora clasifica la foto...", true, null, keyboards.createKeyboard()).then(
-        function(response) {
+                }, function (error) {
+                    sails.log.error("Failed", error);
+                }
+            );
+        } else {
+            telegram.sendMessage(userId, "Ups, eso no me lo esperaba... ¿Te has equivocado?").then(
+                function (response) {
 
-        }, function(error) {
-          sails.log.error("Failed", error);
+                }, function (error) {
+                    sails.log.error("Failed", error);
+                }
+            );
         }
-      );
+        res.json(200);
+
+        /*
+         Updates.create(req.body).exec(function(err, message){
+         if (err) {
+         sails.log.error("Web Hook --> Error", err)
+         }
+         if (message) {
+         sails.log("OK");
+         }
+         })
+         */
     }
-
-    else{
-      telegram.sendMessage(userId, "¡Gracias!").then(
-        function(response) {
-
-        }, function(error) {
-          sails.log.error("Failed", error);
-        }
-      );
-
-    }
-    res.json(200);
-
-    /*
-    Updates.create(req.body).exec(function(err, message){
-      if (err) {
-        sails.log.error("Web Hook --> Error", err)
-      }
-      if (message) {
-        sails.log("OK");
-      }
-    })
-    */
-  }
 };
 
--- api/services/commands.js
+++ api/services/commands.js
@@ -8,34 +8,35 @@
 var Regex = require("regex");
 
 module.exports.processIt = function (text) {
-  var id=0;
-  var command = strip(text);
-  sails.log.debug("Stripped command: ", command);
-  if (command){
-    switch(command)
-    {
-      case "/start":
-        id = 1;
-        break;
-      case "/echo":
-        id = 2;
-        break;
-      default:
-        id = 0;
+    var id = 0;
+    var command = strip(text);
+    sails.log.debug("Stripped command: ", command);
+    if (command) {
+        switch (command) {
+            case "/start":
+                id = 1;
+                break;
+            case "/echo":
+                id = 2;
+                break;
+            default:
+                id = 0;
+        }
+        return id;
     }
-    return id;
-  }
-  else return false;
+    else return false;
 
 };
 
-function strip (text) {
-  var regex = new Regex(/(\/[a-zA-Z]+)/);
-  var array = text.split(" ");
+function strip(text) {
+    var regex = new Regex(/(\/[a-zA-Z]+)/);
+    var array = text.split(" ");
+    sails.log.debug("Array splited: ", array);
 
-  if(regex.test(array[0])){
-    return array[0];
-  }
-  else return false;
+    if (regex.test(array[0])) {
+        sails.log.debug("RETURN REGEX: ", array[0]);
+        return array[0];
+    }
+    else return false;
 
 }
