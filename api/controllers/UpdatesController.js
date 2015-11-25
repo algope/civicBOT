2dedcb741cf407684445a175210038ab0ff5658a
Alejandro <alejandro.gon.per@gmail.com>
2015-11-25 13:18:09 +0100

    Fixes

--- api/controllers/UpdatesController.js
+++ api/controllers/UpdatesController.js
@@ -32,23 +32,23 @@ module.exports = {
             sails.log.info("Photo content: ", update.message.photo);
             telegram.sendMessage(userId, "Ahora clasifica la foto...", "",true, null, null).then(
                 function (response) {
+                    telegram.sendMessage(userId, "Si la información está relacionada con:" +
+                        "```Campañas de comunicación institucionales o con medios de comunicación, *pulsa A*.``" +
+                        "```Acceso y permanencia en el sistema educativo o con el Tercer Sector, *pulsa B*.``" +
+                        "```Reducción de las desigualdades y la exclusión social, *pulsa C*.``" +
+                        "```Transparencia, participación ciudadanía o rendición de cuentas, *pulsa D*.```","Markdown", true, null, keyboards.createKeyboard()).then(
+                        function (response) {
+
+                        }, function (error) {
+                            sails.log.error("Failed", error);
+                        }
+                    )
 
                 }, function (error) {
                     sails.log.error("Failed", error);
                 }
-            ).then(
-                telegram.sendMessage(userId, "Si la información está relacionada con:" +
-                    "```Campañas de comunicación institucionales o con medios de comunicación, *pulsa A*.``" +
-                    "```Acceso y permanencia en el sistema educativo o con el Tercer Sector, *pulsa B*.``" +
-                    "```Reducción de las desigualdades y la exclusión social, *pulsa C*.``" +
-                    "```Transparencia, participación ciudadanía o rendición de cuentas, *pulsa D*.```","Markdown", true, null, keyboards.createKeyboard()).then(
-                    function (response) {
-
-                    }, function (error) {
-                        sails.log.error("Failed", error);
-                    }
-                )
             );
+
         } else {
             telegram.sendMessage(userId, "Ups, eso no me lo esperaba... ¿Te has equivocado?").then(
                 function (response) {
