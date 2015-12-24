5a16e7614604fb7d9996627cedf357980c336324
Alejandro <alejandro.gon.per@gmail.com>
2015-12-24 17:30:12 +0100

    Mixpanel issues

--- api/services/answers.js
+++ api/services/answers.js
@@ -8,7 +8,7 @@
  */
 
 var Mixpanel = require('mixpanel');
-var mixpanel = Mixpanel.init(sails.config.mixpanel.token);
+var mixpanel = Mixpanel.init('2ac0d6d54c481e7dea88d065874c806f');
 
 module.exports.answeringCommandsS1 = function (command, userId, userName) {
     switch (command.commandId) {
