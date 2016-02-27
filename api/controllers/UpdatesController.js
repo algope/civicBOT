aa9ef5a8a41e5fea8df5b4db4163ea5f914fb950
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-02-27 12:44:48 +0100

    testing models

--- api/controllers/UpdatesController.js
+++ api/controllers/UpdatesController.js
@@ -45,99 +45,98 @@ module.exports = {
         //    }
         //});
 
-        sails.log.debug("IT WAS THE DATABASE!");
         /**
          * Let's look for a previous user, otherwise we create a new one
          */
-        stages.findOrCreateEntry({user_id: userId}, {user_id: userId, stage: 1}).then(
-            function (user) {
-                mixpanel.people.set(userId, {
-                    "$first_name": userName,
-                    "$last_name": userLast,
-                    "user_name": userAlias,
-                    "$created": user.createdAt,
-                    "stage": user.stage,
-                    "contributions": 0
-                });
-
-
-                /**
-                 * STAGE 1 > First stage, no commands executed.
-                 */
-                if (user.stage == 1) { //Initial stage
-
-                    if (update.message.photo || command.commandId == 0 || !command) {
-                        answers.answeringError(userId, update, userAlias, user);
-                    } else if (command.commandType == 1) {
-                        answers.answeringCommandsS1(command, userId, userName)
-                    } else {
-                        answers.answeringError(userId, update, userAlias, user);
-                    }
-
-                    /**
-                     * STAGE 2 > Second stage, command /enviar_info executed
-                     */
-                } else if (user.stage == 2) { //Command /enviar_info
-
-                    if (update.message.photo || command.commandId == 0 || !command) {
-                        answers.answeringError(userId, update, userAlias, user);
-                    } else if (command.commandType == 1) {
-                        answers.answeringCommandsS2(command, userId, userName);
-                    } else if (command.commandType == 3) {
-                        answers.answeringTextOrImage(command, userId);
-                    } else {
-                        answers.answeringError(userId, update, userAlias, user);
-                    }
-
-                    /**
-                     * STAGE 3 > Third stage, the user had selected datatype
-                     */
-                } else if (user.stage == 3) { //Data type selected
-
-                    if (command && command.commandType == 1) {
-                        answers.answeringCommandsS3(command, userId, userName);
-                    } else if (update.message.photo && user.data_type_selected == 2) {
-                        answers.answeringLabelingS3(1, update, userId);
-                    } else if (update.message.text && user.data_type_selected == 1) {
-                        answers.answeringLabelingS3(2, update, userId);
-                    } else {
-                        answers.answeringError(userId, update, userAlias, user);
-                    }
-
-
-                    /**
-                     * STAGE 4 > Fourth stage, the bot had recieved the media
-                     */
-                } else if (user.stage == 4) { //Data recieved, not labeled
-
-                    sails.log.debug("commandID: ", command.commandId);
-                    if (update.message.photo || command.commandId == 0 || !command) {
-                        answers.answeringError(userId, update, userAlias, user);
-                    } else if (command.commandType == 1) {
-                        answers.answeringCommandsS4(command, userId, userName);
-                    } else if (command.commandType == 2) {
-                        answers.answeringThanksS4(userId, command, update);
-                    } else {
-                        answers.answeringError(userId, update, userAlias, user);
-                    }
-
-                    /**
-                     * STAGE 10 > Tenth stage (no regular), FeedBack Stage
-                     */
-                } else if (user.stage == 10) { //FeedBack
-
-                    if (update.message.photo || command.commandId == 0) {
-                        answers.answeringError(userId, update, userAlias, user);
-                    } else if (command && command.commandType == 1) {
-                        answers.answeringCommandsS10(command, userId ,userName);
-                    } else if (update.message.text) {
-                        answers.answeringThanksS10(userId, update, userAlias);
-                    } else {
-                        answers.answeringError(userId, update, userAlias, user);
-                    }
-                }
-            }
-        );
+        //stages.findOrCreateEntry({user_id: userId}, {user_id: userId, stage: 1}).then(
+        //    function (user) {
+        //        mixpanel.people.set(userId, {
+        //            "$first_name": userName,
+        //            "$last_name": userLast,
+        //            "user_name": userAlias,
+        //            "$created": user.createdAt,
+        //            "stage": user.stage,
+        //            "contributions": 0
+        //        });
+        //
+        //
+        //        /**
+        //         * STAGE 1 > First stage, no commands executed.
+        //         */
+        //        if (user.stage == 1) { //Initial stage
+        //
+        //            if (update.message.photo || command.commandId == 0 || !command) {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            } else if (command.commandType == 1) {
+        //                answers.answeringCommandsS1(command, userId, userName)
+        //            } else {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            }
+        //
+        //            /**
+        //             * STAGE 2 > Second stage, command /enviar_info executed
+        //             */
+        //        } else if (user.stage == 2) { //Command /enviar_info
+        //
+        //            if (update.message.photo || command.commandId == 0 || !command) {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            } else if (command.commandType == 1) {
+        //                answers.answeringCommandsS2(command, userId, userName);
+        //            } else if (command.commandType == 3) {
+        //                answers.answeringTextOrImage(command, userId);
+        //            } else {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            }
+        //
+        //            /**
+        //             * STAGE 3 > Third stage, the user had selected datatype
+        //             */
+        //        } else if (user.stage == 3) { //Data type selected
+        //
+        //            if (command && command.commandType == 1) {
+        //                answers.answeringCommandsS3(command, userId, userName);
+        //            } else if (update.message.photo && user.data_type_selected == 2) {
+        //                answers.answeringLabelingS3(1, update, userId);
+        //            } else if (update.message.text && user.data_type_selected == 1) {
+        //                answers.answeringLabelingS3(2, update, userId);
+        //            } else {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            }
+        //
+        //
+        //            /**
+        //             * STAGE 4 > Fourth stage, the bot had recieved the media
+        //             */
+        //        } else if (user.stage == 4) { //Data recieved, not labeled
+        //
+        //            sails.log.debug("commandID: ", command.commandId);
+        //            if (update.message.photo || command.commandId == 0 || !command) {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            } else if (command.commandType == 1) {
+        //                answers.answeringCommandsS4(command, userId, userName);
+        //            } else if (command.commandType == 2) {
+        //                answers.answeringThanksS4(userId, command, update);
+        //            } else {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            }
+        //
+        //            /**
+        //             * STAGE 10 > Tenth stage (no regular), FeedBack Stage
+        //             */
+        //        } else if (user.stage == 10) { //FeedBack
+        //
+        //            if (update.message.photo || command.commandId == 0) {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            } else if (command && command.commandType == 1) {
+        //                answers.answeringCommandsS10(command, userId ,userName);
+        //            } else if (update.message.text) {
+        //                answers.answeringThanksS10(userId, update, userAlias);
+        //            } else {
+        //                answers.answeringError(userId, update, userAlias, user);
+        //            }
+        //        }
+        //    }
+        //);
         res.ok();
     }
 };
