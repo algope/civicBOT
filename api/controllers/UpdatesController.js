d91a3e7fb2cacb0a93e46d9b3897e41597e68b80
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-02-27 11:42:28 +0100

    testing models

--- api/controllers/UpdatesController.js
+++ api/controllers/UpdatesController.js
@@ -31,7 +31,7 @@ module.exports = {
         update.message.chat.chat_id = update.message.chat.id;
         delete update.message.chat.id;
         sails.log.debug("UPDATE: : : : : "+update.message.chat);
-        Updates.create(req.body, function (ko, ok) {
+        Updates.create(update, function (ko, ok) {
             if (ko) {
                 sails.log.error("[DB] - Updates.create error: ", err);
             }
