7d7958cd20dc35a64a2a033675559eb9f68237f4
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-02-27 15:36:26 +0100

    Testing models

--- api/models/Admin.js
+++ api/models/Admin.js
@@ -17,7 +17,6 @@ module.exports = {
             type: 'integer',
             primaryKey: true,
             unique: true,
-            required: true,
             autoIncrement: true
         },
 
