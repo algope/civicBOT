c13613e224bd087ccda77008a7307e10564e23ed
Alejandro Gonzalez <alejandro.gon.per@gmail.com>
2016-03-02 23:28:53 +0100

    Enable db redundancy

--- config/env/production.js
+++ config/env/production.js
@@ -24,11 +24,7 @@ module.exports = {
 
         mongodb: {
             adapter: 'sails-mongo',
-            host: process.env.DB_URL,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
+            url: process.env.MONGO_URL
         }
     },
 
